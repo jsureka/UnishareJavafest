ab8f60bf170bdfa8b9d5457706f1ae7d36ce0b53
Md. Inzamam-Ul Haque Sobuz <bsse1113@iit.du.ac.bd>
2023-10-09 02:10:38 +0600

    Pagination added in product apis

--- Server/src/main/java/com/unishare/backend/repository/ProductRepository.java
+++ Server/src/main/java/com/unishare/backend/repository/ProductRepository.java
@@ -1,8 +1,6 @@
 package com.unishare.backend.repository;
 
 import com.unishare.backend.model.Product;
-import com.unishare.backend.model.University;
-import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,13 +12,29 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Product findProductById(Long id);
-    List<Product> findAllByCategoryId(Long categoryId);
-    List<Product> findAllByOwnerId(Long categoryId);
-    List<Product> findAllByStatus(String status);
-    List<Product> findAllByOwnerIdAndCategoryId(Long ownerId, Long categoryId);
-    List<Product> findAllByOwnerIdAndStatus(Long ownerId, String status);
-    List<Product> findAllByCategoryIdAndStatus(Long categoryId, String status);
-    List<Product> findAllByOwnerIdAndStatusAndCategoryId(Long ownerId, String status, Long categoryId);
+
+    List<Product> findAllByOwnerId(Long ownerId);
+
+    @Query(value = "SELECT p FROM Product p WHERE p.category.id = :categoryId")
+    Page<Product> findAllByCategoryId(Long categoryId, Pageable pageable);
+
+    @Query(value = "SELECT p FROM Product p WHERE p.owner.id = :ownerId")
+    Page<Product> findAllByOwnerId(Long ownerId, Pageable pageable);
+
+    @Query(value = "SELECT p FROM Product p WHERE p.status = :status")
+    Page<Product> findAllByStatus(String status, Pageable pageable);
+
+    @Query(value = "SELECT p FROM Product p WHERE p.owner.id = :ownerId AND p.category.id = :categoryId")
+    Page<Product> findAllByOwnerIdAndCategoryId(Long ownerId, Long categoryId, Pageable pageable);
+
+    @Query(value = "SELECT p FROM Product p WHERE p.owner.id = :ownerId AND p.status = :status")
+    Page<Product> findAllByOwnerIdAndStatus(Long ownerId, String status, Pageable pageable);
+
+    @Query(value = "SELECT p FROM Product p WHERE p.category.id = :categoryId AND p.status = :status")
+    Page<Product> findAllByCategoryIdAndStatus(Long categoryId, String status, Pageable pageable);
+
+    @Query(value = "SELECT p FROM Product p WHERE p.owner.id = :ownerId AND p.category.id = :categoryId AND p.status = :status")
+    Page<Product> findAllByOwnerIdAndStatusAndCategoryId(Long ownerId, String status, Long categoryId, Pageable pageable);
 
     @Query(value = "SELECT p FROM Product p")
     Page<Product> getProductsPage(final Pageable pageable);
