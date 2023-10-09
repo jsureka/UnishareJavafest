276ffd0fb7f0bd0d19cbeae589be9faae7625924
Jitesh Sureka <bsse1115@iit.du.ac.bd>
2023-10-10 01:35:52 +0600

    routes

--- src/app/dashboard/owner/layout.js
+++ src/app/dashboard/owner/layout.js
@@ -1,29 +1,29 @@
 "use client";
-import ProtectedRoute from "@/authGaurd/ProtectedRoutes";
 import Navbar from "@/components/BorrowerComponents/Navbar/Navbar";
 import Sidebar from "@/components/OwnerComponents/Sidebar";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 const layout = ({ children }) => {
-  const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+  const currentUser = useSelector((state) => state.user.currentUser);
   const router = useRouter();
-  if (!isAuthenticated) {
-    router.push("/login");
-  }
+  useEffect(() => {
+    if (!currentUser) {
+      router.push("/login");
+    }
+  }, [currentUser, router]);
   return (
     <>
-      <ProtectedRoute>
-        <Navbar />
-        <div className="min-h-screen flex flex-col">
-          <div className="flex flex-col md:flex-row flex-1">
-            <aside className=" w-full  dark:bg-gray-700 md:w-60">
-              <Sidebar />
-            </aside>
-            <main className="flex-1">{children}</main>
-          </div>
+      <Navbar />
+      <div className="min-h-screen flex flex-col">
+        <div className="flex flex-col md:flex-row flex-1">
+          <aside className=" w-full  dark:bg-gray-700 md:w-60">
+            <Sidebar />
+          </aside>
+          <main className="flex-1">{children}</main>
         </div>
-      </ProtectedRoute>
+      </div>
     </>
   );
 };
--- src/authGaurd/ProtectedRoutes.jsx
+++ src/authGaurd/ProtectedRoutes.jsx
@@ -6,13 +6,17 @@ import { useSelector } from "react-redux"; // or your preferred state management
 
 const ProtectedRoute = ({ children }) => {
   const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+  const user = useSelector((state) => state.user.currentUser);
   const router = useRouter();
 
   useEffect(() => {
     if (!isAuthenticated && !localStorage.getItem("jwt_token")) {
       router.push("/login");
     }
-  }, []);
+    if (isAuthenticated && user?.role !== "ADMIN") {
+      router.push("/dashboard/storefront/home");
+    }
+  }, [isAuthenticated, user, router]);
 
   return <>{children}</>;
 };
